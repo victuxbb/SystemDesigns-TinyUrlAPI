59d4dc34a96ee99514b2b7a226354188d5f55d31
Victor Caldentey <victuxbb@gmail.com>
2019-02-07 19:28:49 +0100

    Initial poc

---
+++ src/test/java/com/victuxbb/systemdesigns/tinyurlapi/TinyurlapiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.victuxbb.systemdesigns.tinyurlapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinyurlapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
