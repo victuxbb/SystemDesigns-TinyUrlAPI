41215a98afa8d54a2f42afc27b7dc75839d00665
Victor <victuxbb@gmail.com>
2019-02-08 18:08:46 +0100

    Feature/initial structure (#1)
    * Initial poc
    
    * Add docker configuration to run tests inside travis
    
    * Add badget travis build
    
    * Remove gradle folder that should be ignored
    
    * ignore gradle folder
    
    * Fix install section
    
    * improve git ignore
    
    https://github.com/github/gitignore/blob/master/Gradle.gitignore

---
+++ src/main/java/com/victuxbb/systemdesigns/tinyurlapi/TinyurlapiApplication.java
@@ -0,0 +1,14 @@
+package com.victuxbb.systemdesigns.tinyurlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurlapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyurlapiApplication.class, args);
+	}
+
+}
+
---
+++ src/main/resources/cassandra-init.sh
@@ -0,0 +1,7 @@
+# You may add some other conditionals that fits your stuation here
+until cqlsh -f /import.cql; do
+  echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
+  sleep 2
+done &
+
+exec /docker-entrypoint.sh "$@"
