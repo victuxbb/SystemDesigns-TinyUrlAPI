3d4d67a51f982e112ac0edd5ba76c8cd6053d387
Arkioner <ioner468@gmail.com>
2019-02-10 00:30:08 +0100

    better key generator

--- src/main/java/com/victuxbb/systemdesigns/tinyurlapi/infrastructure/datasource/InMemoryKGSRepository.java
+++ src/main/java/com/victuxbb/systemdesigns/tinyurlapi/infrastructure/datasource/InMemoryKGSRepository.java
@@ -5,10 +5,12 @@ import com.victuxbb.systemdesigns.tinyurlapi.domain.kgs.URLKey;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @Repository
 public class InMemoryKGSRepository implements KGSRepository {
     @Override
     public Mono<URLKey> getUniqueKey() {
-        return Mono.just(new URLKey("/miau"));
+        return Mono.just(new URLKey(UUID.randomUUID().toString()));
     }
 }
